92c92eac39264988ab9392077b9d0b93cdae7ddb
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-04-29 20:06:49 +0300

    Добавьте файлы проекта.

---
+++ OopSpecificsInheritanceProject/OopSpecificsInheritanceProject.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+class Base
+{
+private:
+    int value_private;
+protected:
+    int value_protected;
+public:
+    int value_public;
+};
+
+class PublicDerived : public Base
+{  
+private:
+    void method()
+    {
+        value_protected; // prodected
+        value_public;    // public
+    }
+protected:
+public:
+};
+
+class PrivateDerived : private Base
+{
+private:
+    void method()
+    {
+        value_protected; // private
+        value_public;    // private
+    }
+protected:
+public:
+};
+
+class PrivateDerived : protected Base
+{
+private:
+    void method()
+    {
+        value_protected; // protected
+        value_public;    // protected
+    }
+protected:
+public:
+};
+
+int main()
+{
+    
+}
+
