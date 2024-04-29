1d2288c7e1d2f24401b4f27ce1675e4f726418b0
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-04-29 21:20:28 +0300

    29-04-24

--- OopSpecificsInheritanceProject/OopSpecificsInheritanceProject.cpp
+++ OopSpecificsInheritanceProject/OopSpecificsInheritanceProject.cpp
@@ -8,6 +8,7 @@ protected:
     int value_protected;
 public:
     int value_public;
+    virtual void Method() { std::cout << "Base\n"; }
 };
 
 class PublicDerived : public Base
@@ -34,20 +35,111 @@ protected:
 public:
 };
 
-class PrivateDerived : protected Base
+//class PrivateDerived : protected Base
+//{
+//private:
+//    void method()
+//    {
+//        value_protected; // protected
+//        value_public;    // protected
+//    }
+//protected:
+//public:
+//};
+
+
+
+class DerivedA : public Base
 {
-private:
-    void method()
-    {
-        value_protected; // protected
-        value_public;    // protected
-    }
-protected:
 public:
+    void Method() { std::cout << "Derived A\n"; }
 };
+
+class DerivedB : public DerivedA
+{
+public:
+    //void Method() { std::cout << "Derived B\n"; }
+};
+
+class DerivedC : public DerivedB
+{
+public:
+    //void Method() { std::cout << "Derived C\n"; }
+};
+
+
+class Gaget
+{
+protected:
+    std::string name;
+public:
+    Gaget(std::string name) : name{ name }
+    { 
+        std::cout << "Gaget " << this << "\n"; 
+    }
+    virtual void NamePrint()
+    {
+        std::cout << "Gaget name: " << name << "\n";
+    }
+};
+
+class Printer : public Gaget
+{
+public:
+    Printer(std::string name) : Gaget(name)
+    { 
+        std::cout << "Printer " << this << "\n"; 
+    }
+    void Work() { std::cout << "Print\n"; }
+    void NamePrint() override
+    {
+        std::cout << "Printer name: " << name << "\n";
+    }
+};
+
+class Scaner : public Gaget
+{
+public:
+    Scaner(std::string name) : Gaget(name)
+    { 
+        std::cout << "Scaner " << this << "\n"; 
+    }
+    void Work() { std::cout << "Scan\n"; }
+    void NamePrint() override
+    {
+        std::cout << "Scaner name: " << name << "\n";
+    }
+};
+
+class Mfu : public Printer, public Scaner
+{
+public:
+    Mfu(std::string namePrinter, std::string nameScaner) 
+        : Printer(namePrinter), Scaner(nameScaner)
+    { 
+        std::cout << "Mfu " << this << "\n"; 
+    }
+    void NamePrint() override
+    {
+        std::cout << "Mfu name: " << Scaner::name << "\n";
+    }
+};
+
 
 int main()
 {
-    
+    Base* basePtr = new Base();
+    basePtr->Method();
+
+    basePtr = new DerivedA();
+    basePtr->Method();
+
+    basePtr = new DerivedC();
+    basePtr->Method();
+
+    Mfu mfu;
+    mfu.Scaner::Work();
+    mfu.Printer::Work();
+   
 }
 
